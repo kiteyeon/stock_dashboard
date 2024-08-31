0e208120aeb7507e3c1bb5564b530134acbc4a6a
kiteyeon <dusrudal3@gmail.com>
2024-08-31 10:43:37 +0900

    날짜 범위 선택 및 선택된 날짜 데이터 표시하는 테이블 추가

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-import streamlit as st
+import pandas as pd
 
+import streamlit as st
 from data_loader import load_data
 
 def main():
-    st.title('This is dashboard')
+st.title('This is dashboard')
 
-    df = load_data()
+df = load_data()
 
-    st.write(df)
+# 특정 날짜 범위 선택
+st.subheader("Select Date Range")
+df['Date'] = pd.to_datetime(df['Date'])
+start_date = st.date_input("Start date", df['Date'].min())
+end_date = st.date_input("End date", df['Date'].max())
+
+ranged_df = df[df(df['Date']>=pd/to_datetime(start_date))&(df['Date']<=pd.to_datetime(end_date))]
+ranged_df = ranged_df.reset_index(drop=True)
+st.table(ranged_df)
 
 if __name__ == '__main__' :
     main()
